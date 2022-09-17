aa36ed573b19d376965aaf2906d3ba93ef87099f
muratdemirci <6681137+muratdemirci@users.noreply.github.com>
2022-09-17 07:06:00 +0300

    now we have much better error message

--- src/api/v1/controllers/auth/index.js
+++ src/api/v1/controllers/auth/index.js
@@ -5,25 +5,25 @@ const asyncHandler = require('../../middleware/async')
 // @route     POST /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
-    const { name, email, password } = req.body
-    console.log({ name, email, password })
-    // Create user
-    const user = await User.create({
-        name,
-        email,
-        password,
+  const { name, email, password } = req.body
+  console.log({ name, email, password })
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+    .then(function (user) {
+      res.json({
+        Message: 'User created',
+        User: user,
+      })
     })
-        .then(function (user) {
-            res.json({
-                Message: 'User created',
-                User: user,
-            })
-        })
-        .catch(function (err) {
-            console.log(err)
-        })
+    .catch(function (err) {
+      console.log(err)
+    })
 
-    // console.log(user)
+  // console.log(user)
 
-    // TODO: send token response
+  // TODO: send token response
 })
--- src/api/v1/models/User.js
+++ src/api/v1/models/User.js
@@ -1,24 +1,31 @@
 const Sequelize = require('sequelize')
 const db = require('../../../config/database')
 
-const User = db.define('users', {
+const User = db.define(
+  'users',
+  {
     id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     name: {
-        type: Sequelize.STRING,
-        allowNull: false,
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     email: {
-        type: Sequelize.STRING,
-        allowNull: false,
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     password: {
-        type: Sequelize.STRING,
-        allowNull: false,
+      type: Sequelize.STRING,
+      allowNull: false,
     },
-})
+  },
+  {
+    freezeTableName: true,
+    tableName: 'users',
+  }
+)
 
 module.exports = User
