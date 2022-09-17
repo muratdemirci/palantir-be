6a5ac713bac693e36d0cb42f9cba744201148ec6
muratdemirci <6681137+muratdemirci@users.noreply.github.com>
2022-09-17 14:38:37 +0300

    analytics end point init.

--- src/api/v1/routes/analytics/index.js
+++ src/api/v1/routes/analytics/index.js
@@ -1,2 +1,11 @@
 const express = require('express')
 // anaylsis endpoint should be protected
+
+//TODO: analytics
+const express = require('express')
+const { insert, list } = require('../../controllers/analytics')
+
+const router = express.Router()
+
+router.post('/insert', insert)
+router.get('/list', list)
