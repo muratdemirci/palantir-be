7e0562b5b46dd487ea49e55c072f5df2fad92ccc
muratdemirci <6681137+muratdemirci@users.noreply.github.com>
2022-09-17 13:36:31 +0300

    add user register and user login

--- src/api/v1/controllers/auth/index.js
+++ src/api/v1/controllers/auth/index.js
@@ -1,29 +1,69 @@
 const User = require('../../models/User')
 const asyncHandler = require('../../middleware/async')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 // @desc      Register user
 // @route     POST /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body
-  console.log({ name, email, password })
   // Create user
   const user = await User.create({
     name,
     email,
-    password,
+    password: bcrypt.hashSync(password, 8),
   })
     .then(function (user) {
       res.json({
-        Message: 'User created',
+        Message: 'User registered successfully!',
         User: user,
       })
     })
     .catch(function (err) {
-      console.log(err)
+      Logger.log('Error:', `${JSON.stringify(err)}`)
+      return next(new ErrorResponse('User could not registered!', 400))
     })
-
-  // console.log(user)
-
-  // TODO: send token response
 })
+
+exports.login = (req, res) => {
+  const { email } = req.body
+  const secret = process.env.SECRET_KEY || 'mordoridmanyurdu'
+
+  User.findOne({
+    where: {
+      email,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'User Not found.' })
+      }
+
+      const passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      )
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: 'Invalid Password!',
+        })
+      } else {
+        const token = jwt.sign({ id: user.id }, secret, {
+          expiresIn: 86400, // 24 hours
+        })
+
+        res.status(200).send({
+          id: user.id,
+          username: user.name,
+          email: user.email,
+          accessToken: token,
+        })
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message })
+    })
+}
